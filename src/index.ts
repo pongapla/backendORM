45143b825ad905c1a018108a3685c474e5d18057
pong <kittiphob.m@ns.co.th>
2024-01-09 12:32:43 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT ?? 4000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript Express!");
